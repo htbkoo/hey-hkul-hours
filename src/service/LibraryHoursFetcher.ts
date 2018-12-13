40ee5b56a3b9dea3205b680bdf5834a17037d489
htbkoo <h1017760@connect.hku.hk>
2018-12-14 00:15:58 +0800

    implemented html parsing using jsdom htbkoo/hey-hkul-hours

--- src/service/LibraryHoursFetcher.ts
+++ src/service/LibraryHoursFetcher.ts
@@ -1,3 +1,5 @@
+import {JSDOM} from "jsdom";
+
 import HtmlFetcher from "../external/HtmlFetcher";
 
 export default class LibraryHoursFetcher {
@@ -8,6 +10,37 @@ export default class LibraryHoursFetcher {
     }
 
     async retrieveHours() {
-        return await this._htmlFetcher.fetchHtml("");
+        const html = await this._htmlFetcher.fetchHtml("");
+        return this.parseHtml(html);
+    }
+
+    private parseHtml(html) {
+        return getAllRows()
+            .map(rowToTableCells)
+            .map(tableCellsToStrings)
+            .reduce((obj, {libraryName, hour}) => {
+                obj[libraryName as any] = hour;
+                return obj;
+            }, {});
+
+        function getAllRows() {
+            const dom = new JSDOM(html);
+            return Array.from(dom.window.document.querySelectorAll("tr"));
+        }
+
+        function rowToTableCells(row) {
+            return Array.from((row as any).querySelectorAll("td"))
+        }
+
+        function tableCellsToStrings(tableCells) {
+            return {
+                libraryName: cellAsString(tableCells[0]),
+                hour: cellAsString(tableCells[1]),
+            };
+
+            function cellAsString(cell: any) {
+                return cell.textContent;
+            }
+        }
     }
 }
