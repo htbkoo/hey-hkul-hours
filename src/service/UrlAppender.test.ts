0ec699f6bbb7581772718a3562b3b9a0d46aefd2
htbkoo <h1017760@connect.hku.hk>
2018-12-14 23:18:38 +0800

    added possibility to override date format in UrlAppender.ts htbkoo/hey-hkul-hours

--- src/service/UrlAppender.test.ts
+++ src/service/UrlAppender.test.ts
@@ -30,4 +30,15 @@ describe("HtmlParser", function () {
             expect(actualUrl).toEqual(combinedUrl);
         })
     );
+
+    it("should be possible to override date format in string via an optional dateFormat constructor argument", function () {
+        // given
+        const urlAppender = new UrlAppender("prefix ", " postfix", "[my format] MMMM Do YYYY, h:mm:ss a");
+
+        // when
+        const actualUrl = urlAppender.buildUrlWithDate(moment("2013-02-08 09:30:26.123"));
+
+        // then
+        expect(actualUrl).toEqual("prefix my format February 8th 2013, 9:30:26 am postfix");
+    });
 });
--- src/service/UrlAppender.ts
+++ src/service/UrlAppender.ts
@@ -6,13 +6,15 @@ const DEFAULT_DATE_FORMAT = "YYYY-MM-DD";
 export default class UrlAppender {
     private _prefix: string;
     private _postfix: string;
+    private _format: string;
 
-    constructor(prefix: string, postfix: string) {
+    constructor(prefix: string, postfix: string, format: string = DEFAULT_DATE_FORMAT) {
         this._prefix = prefix;
         this._postfix = postfix;
+        this._format = format;
     }
 
     buildUrlWithDate(date: Moment): string {
-        return `${this._prefix}${date.format(DEFAULT_DATE_FORMAT)}${this._postfix}`;
+        return `${this._prefix}${date.format(this._format)}${this._postfix}`;
     }
 }
