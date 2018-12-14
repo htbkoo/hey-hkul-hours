794eb2c6d92bfb976e2b2113896c10f618ccb714
htbkoo <h1017760@connect.hku.hk>
2018-12-15 00:25:22 +0800

    (WIP) made HourParser.test.ts more generic so that it could test the case for HourSimpleParser as well htbkoo/hey-hkul-hours

---
+++ src/service/HourParser.test.ts
@@ -0,0 +1,61 @@
+import {when} from 'jest-when';
+
+import HourRegexParser from "./HourRegexParser";
+import HourParser from "./HourParser";
+
+describe("HourParser", function () {
+    [
+        {parserName: "HourRegexParser", parser: new HourRegexParser()},
+    ].forEach(({parserName, parser}: { parserName: string, parser: HourParser }) => {
+        describe(parserName, function () {
+            [
+                {input: "8:15am - 11:00pm", expected: [{from: "8:15am", to: "11:00pm"}]},
+                {
+                    input: "8:30am - 6:00am of the following day",
+                    expected: [{from: "8:30am", to: "6:00am of the following day"}]
+                },
+                {input: "8:30am - 10:30pm", expected: [{from: "8:30am", to: "10:30pm"}]},
+                {input: "8:30am - 11:00pm", expected: [{from: "8:30am", to: "11:00pm"}]},
+                {input: "9:00am - 8:00pm", expected: [{from: "9:00am", to: "8:00pm"}]},
+                {input: "9:00am - 9:00pm", expected: [{from: "9:00am", to: "9:00pm"}]},
+                {input: "9:00am - 10:00pm", expected: [{from: "9:00am", to: "10:00pm"}]},
+
+                {input: "8:15am- 11:00pm", expected: [{from: "8:15am", to: "11:00pm"}]},
+                {input: "8:15am -11:00pm", expected: [{from: "8:15am", to: "11:00pm"}]},
+                {input: "8:15am-11:00pm", expected: [{from: "8:15am", to: "11:00pm"}]},
+                {input: " 8:15am-11:00pm ", expected: [{from: "8:15am", to: "11:00pm"}]},
+                {input: " 8:15am-11:00pm", expected: [{from: "8:15am", to: "11:00pm"}]},
+                {input: "8:15am-11:00pm ", expected: [{from: "8:15am", to: "11:00pm"}]},
+                {input: " 8:15am - 11:00pm ", expected: [{from: "8:15am", to: "11:00pm"}]},
+            ].forEach(({input, expected}) =>
+                it(`should parse "${input}" from string into parts`, async function () {
+                    // given
+                    // when
+                    const hourInString = parser.parse(input);
+
+                    // then
+                    expect(hourInString).toEqual(expected);
+                })
+            );
+
+            [
+                "unparsable string",
+                "8:15am - ",
+                "8:15am- ",
+                "8:15am -",
+                "8:15am-",
+                " -11:00pm",
+                "- 11:00pm",
+                "-11:00pm",
+            ].forEach(input =>
+                it(`should handle unparsable input "${input}" gracefully`, function () {
+                    // given
+                    // when
+                    expect(() => parser.parse(input))
+                    // then
+                        .toThrow(`Unable to parse input: "${input}"`);
+                })
+            );
+        });
+    });
+});
--- src/service/HourRegexParser.test.ts
+++ src/service/HourRegexParser.test.ts
@@ -3,35 +3,6 @@ import {when} from 'jest-when';
 import HourRegexParser from "./HourRegexParser";
 
 describe("HourRegexParser", function () {
-    [
-        {input: "8:15am - 11:00pm", expected: [{from: "8:15am", to: "11:00pm"}]},
-        {input: "8:30am - 6:00am of the following day", expected: [{from: "8:30am", to: "6:00am of the following day"}]},
-        {input: "8:30am - 10:30pm", expected: [{from: "8:30am", to: "10:30pm"}]},
-        {input: "8:30am - 11:00pm", expected: [{from: "8:30am", to: "11:00pm"}]},
-        {input: "9:00am - 8:00pm", expected: [{from: "9:00am", to: "8:00pm"}]},
-        {input: "9:00am - 9:00pm", expected: [{from: "9:00am", to: "9:00pm"}]},
-        {input: "9:00am - 10:00pm", expected: [{from: "9:00am", to: "10:00pm"}]},
-
-        {input: "8:15am- 11:00pm", expected: [{from: "8:15am", to: "11:00pm"}]},
-        {input: "8:15am -11:00pm", expected: [{from: "8:15am", to: "11:00pm"}]},
-        {input: "8:15am-11:00pm", expected: [{from: "8:15am", to: "11:00pm"}]},
-        {input: " 8:15am-11:00pm ", expected: [{from: "8:15am", to: "11:00pm"}]},
-        {input: " 8:15am-11:00pm", expected: [{from: "8:15am", to: "11:00pm"}]},
-        {input: "8:15am-11:00pm ", expected: [{from: "8:15am", to: "11:00pm"}]},
-        {input: " 8:15am - 11:00pm ", expected: [{from: "8:15am", to: "11:00pm"}]},
-    ].forEach(({input, expected}) =>
-        it(`should parse "${input}" from string into parts`, async function () {
-            // given
-            const parser = new HourRegexParser();
-
-            // when
-            const hourInString = parser.parse(input);
-
-            // then
-            expect(hourInString).toEqual(expected);
-        })
-    );
-
     it("should be possible to override hour format through constructor argument", function () {
         // given
         const parser = new HourRegexParser(/(.+)-(.+)/);
@@ -45,25 +16,4 @@ describe("HourRegexParser", function () {
             to: "11:00pm of next day",
         }]);
     });
-
-    [
-        "unparsable string",
-        "8:15am - ",
-        "8:15am- ",
-        "8:15am -",
-        "8:15am-",
-        " -11:00pm",
-        "- 11:00pm",
-        "-11:00pm",
-    ].forEach(input =>
-        it(`should handle unparsable input "${input}" gracefully`, function () {
-            // given
-            const parser = new HourRegexParser();
-
-            // when
-            expect(() => parser.parse(input))
-            // then
-                .toThrow(`Unable to parse input: "${input}"`);
-        })
-    );
 });
