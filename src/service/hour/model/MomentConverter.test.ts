adb2a13e076830f5def39ecfc4a5061e58d5be2c
htbkoo <h1017760@connect.hku.hk>
2018-12-15 18:34:21 +0800

    added parsing for day of the following day htbkoo/hey-hkul-hours

--- src/service/hour/model/MomentConverter.test.ts
+++ src/service/hour/model/MomentConverter.test.ts
@@ -3,7 +3,9 @@ import {hour} from "../../../tests/utils/HourUtils";
 
 describe("MomentConverter", function () {
     [
-        {str: "8:30am", expected: hour("8:30am")}
+        {str: "8:30am", expected: hour("8:30am")},
+        {str: "8:15pm", expected: hour("8:15pm")},
+        {str: "6:00am of the following day", expected: hour("6:00am").add(1, "d")},
     ].forEach(({str, expected}) =>
         it(`should convert string "${str}" to moment"`, async function () {
             // given
--- src/service/hour/model/MomentConverter.ts
+++ src/service/hour/model/MomentConverter.ts
@@ -1,10 +1,36 @@
 import * as moment from "moment";
 import {Moment} from "moment";
 
+const FOLLOWING_DAY_REGEX = /(.+)of the following day/;
 const DEFAULT_TIME_FORMAT = "h:ma";
 
 export default class MomentConverter {
     convert(str: string): Moment {
-        return moment(str, DEFAULT_TIME_FORMAT);
+        const parseResult = parsingAsFollowingDay();
+
+        if (parseResult.isFollowingDay()) {
+            return parseResult.asMoment();
+        } else {
+            return moment(str, DEFAULT_TIME_FORMAT);
+        }
+
+        function parsingAsFollowingDay() {
+            const result = FOLLOWING_DAY_REGEX.exec(str);
+            return {
+                isFollowingDay() {
+                    return result !== null && isNonEmpty(this.getTime());
+                },
+                asMoment() {
+                    return moment(this.getTime(), DEFAULT_TIME_FORMAT).add(1, "d");
+                },
+                getTime(): string {
+                    return result[1];
+                }
+            };
+
+            function isNonEmpty(rawStr: string) {
+                return rawStr && rawStr.trim() !== "";
+            }
+        }
     }
 }
