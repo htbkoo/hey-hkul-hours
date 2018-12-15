a9732994dacf5dbbb83b1a46e5751fb40b022515
htbkoo <h1017760@connect.hku.hk>
2018-12-15 15:27:16 +0800

    fixed wrong interface name in HourParser.ts htbkoo/hey-hkul-hours

--- src/service/hour/HourParser.ts
+++ src/service/hour/HourParser.ts
@@ -7,6 +7,6 @@ export function unparsableStringToHourError(str: string){
     return new TypeError(`Unable to parse input: "${str}"`)
 }
 
-export default interface RegexHourParser {
+export default interface HourParser {
     parse(str: string): HourInString
 }
