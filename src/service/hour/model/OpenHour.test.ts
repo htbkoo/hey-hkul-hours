477515b55779d3c5621f1ebfb641e53bb01b3ab8
htbkoo <h1017760@connect.hku.hk>
2018-12-18 19:40:37 +0800

    refactored OpenHour.test.ts to group test cases into suites htbkoo/hey-hkul-hours

--- src/service/hour/model/OpenHour.test.ts
+++ src/service/hour/model/OpenHour.test.ts
@@ -4,52 +4,56 @@ import OpenHour from "./OpenHour";
 import {hour} from "../../../tests/utils/HourUtils";
 
 describe("OpenHour", function () {
-    it(`should be able to create open hour`, async function () {
-        // given
-        const from = hour("8:30am"), to = hour("11:00pm");
+    describe("creation", function () {
+        it(`should be able to create open hour`, async function () {
+            // given
+            const from = hour("8:30am"), to = hour("11:00pm");
 
-        // when
-        const openHour = new OpenHour({from, to});
+            // when
+            const openHour = new OpenHour({from, to});
 
-        // then
-        expect(openHour.isClosed()).toEqual(false);
-        expect(openHour.getFrom()).toEqual(from);
-        expect(openHour.getTo()).toEqual(to);
+            // then
+            expect(openHour.isClosed()).toEqual(false);
+            expect(openHour.getFrom()).toEqual(from);
+            expect(openHour.getTo()).toEqual(to);
+        });
     });
 
-    it("should test that 2 same OpenHours are equal to each other", function () {
-        // given
-        const from = hour("8:30am"), to = hour("11:00pm");
+    describe("equality", function () {
+        it("should test that 2 same OpenHours are equal to each other", function () {
+            // given
+            const from = hour("8:30am"), to = hour("11:00pm");
 
-        // when
-        const openHour1 = new OpenHour({from, to}), openHour2 = new OpenHour({from, to});
+            // when
+            const openHour1 = new OpenHour({from, to}), openHour2 = new OpenHour({from, to});
 
-        // then
-        assertOpenHour(openHour1).toEqual(openHour2);
-    });
+            // then
+            assertOpenHour(openHour1).toEqual(openHour2);
+        });
 
-    it("should test that 2 different OpenHours are not equal to each other", function () {
-        // given
-        const from1 = hour("8:30am"), to1 = hour("11:00pm");
-        const from2 = hour("8:31am"), to2 = hour("11:00pm");
+        it("should test that 2 different OpenHours are not equal to each other", function () {
+            // given
+            const from1 = hour("8:30am"), to1 = hour("11:00pm");
+            const from2 = hour("8:31am"), to2 = hour("11:00pm");
 
-        // when
-        const openHour1 = new OpenHour({from: from1, to: to1}), openHour2 = new OpenHour({from: from2, to: to2});
+            // when
+            const openHour1 = new OpenHour({from: from1, to: to1}), openHour2 = new OpenHour({from: from2, to: to2});
 
-        // then
-        assertOpenHour(openHour1).not.toEqual(openHour2);
-    });
+            // then
+            assertOpenHour(openHour1).not.toEqual(openHour2);
+        });
 
-    it("should test that cloned OpenHour is equal to OpenHour", function () {
-        // given
-        const from = hour("8:30am"), to = hour("11:00pm");
-        const openHour1 = new OpenHour({from, to});
+        it("should test that cloned OpenHour is equal to OpenHour", function () {
+            // given
+            const from = hour("8:30am"), to = hour("11:00pm");
+            const openHour1 = new OpenHour({from, to});
 
-        // when
-        const openHour2 = openHour1.clone();
+            // when
+            const openHour2 = openHour1.clone();
 
-        // then
-        assertOpenHour(openHour1).toEqual(openHour2);
+            // then
+            assertOpenHour(openHour1).toEqual(openHour2);
+        });
     });
 
     function assertOpenHour(openHour1: OpenHour) {
