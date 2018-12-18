c54ea19f9d989d04e78431b0ad2850c8caf2c1e7
htbkoo <h1017760@connect.hku.hk>
2018-12-18 19:47:15 +0800

    refactored Hours.test.ts to group test cases into suites htbkoo/hey-hkul-hours

--- src/service/hour/model/Hours.test.ts
+++ src/service/hour/model/Hours.test.ts
@@ -5,27 +5,31 @@ import OpenHour from "./OpenHour";
 import {hour} from "../../../tests/utils/HourUtils";
 
 describe("Hours", function () {
-    it(`should be able to create open hours`, async function () {
-        // given
-        const hour1 = new OpenHour({from: hour("8:30am"), to: hour("8:30am")}),
-            hour2 = new OpenHour({from: hour("8:30am"), to: hour("8:30am")});
+    describe("open hours", function () {
+        it(`should be able to create open hours`, async function () {
+            // given
+            const hour1 = new OpenHour({from: hour("8:30am"), to: hour("8:30am")}),
+                hour2 = new OpenHour({from: hour("8:30am"), to: hour("8:30am")});
 
-        // when
-        const openHours = [hour1, hour2];
-        const hours = Hours.openHours(openHours);
+            // when
+            const openHours = [hour1, hour2];
+            const hours = Hours.openHours(openHours);
 
-        // then
-        expect(hours.isClosed()).toEqual(false);
-        expect(hours.asArray()).toEqual(openHours)
+            // then
+            expect(hours.isClosed()).toEqual(false);
+            expect(hours.asArray()).toEqual(openHours)
+        });
     });
 
-    it(`should be able to create closed hours`, async function () {
-        // given
-        // when
-        const hours = Hours.closed();
+    describe("closed hours", function () {
+        it(`should be able to create closed hours`, async function () {
+            // given
+            // when
+            const hours = Hours.closed();
 
-        // then
-        expect(hours.isClosed()).toEqual(true);
-        expect(hours.asArray()).toEqual([]);
+            // then
+            expect(hours.isClosed()).toEqual(true);
+            expect(hours.asArray()).toEqual([]);
+        });
     });
 });
