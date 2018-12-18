5bdc1ebabae1ce5c59dcd0770eac85515cdd73e8
htbkoo <h1017760@connect.hku.hk>
2018-12-18 19:50:18 +0800

    parameterized and add more test cases in Hours.test.ts htbkoo/hey-hkul-hours

--- src/service/hour/model/Hours.test.ts
+++ src/service/hour/model/Hours.test.ts
@@ -5,20 +5,25 @@ import OpenHour from "./OpenHour";
 import {hour} from "../../../tests/utils/HourUtils";
 
 describe("Hours", function () {
+    const hour1 = new OpenHour({from: hour("8:30am"), to: hour("9:30am")}),
+        hour2 = new OpenHour({from: hour("8:30pm"), to: hour("9:30pm")});
+
     describe("open hours", function () {
-        it(`should be able to create open hours`, async function () {
-            // given
-            const hour1 = new OpenHour({from: hour("8:30am"), to: hour("8:30am")}),
-                hour2 = new OpenHour({from: hour("8:30am"), to: hour("8:30am")});
+        [
+            {scenario: "1 open hour", openHours: [hour1]},
+            {scenario: "2 open hours", openHours: [hour1, hour2]},
+            {scenario: "2 open hours in different order", openHours: [hour2, hour1]},
+        ].forEach(({scenario, openHours}) =>
+            it(`should be able to create open hours for ${scenario}`, async function () {
+                // given
+                // when
+                const hours = Hours.openHours(openHours);
 
-            // when
-            const openHours = [hour1, hour2];
-            const hours = Hours.openHours(openHours);
-
-            // then
-            expect(hours.isClosed()).toEqual(false);
-            expect(hours.asArray()).toEqual(openHours)
-        });
+                // then
+                expect(hours.isClosed()).toEqual(false);
+                expect(hours.asArray()).toEqual(openHours)
+            })
+        );
     });
 
     describe("closed hours", function () {
