24dd7679b5e18939a19c756d8d2a246270c15ebb
htbkoo <h1017760@connect.hku.hk>
2018-12-19 21:23:17 +0800

    grouped tests into suites in HoursConverter.test.ts htbkoo/hey-hkul-hours

--- src/service/HoursConverter.test.ts
+++ src/service/HoursConverter.test.ts
@@ -3,21 +3,23 @@ import {when} from 'jest-when';
 import HoursConverter from "./HoursConverter";
 
 describe("HoursConverter", function () {
-    [
-        "CLOSED",
-        "closed",
-        "Closed",
-    ].forEach(str =>
-        it(`should be able to recognized "${str}" and will return hours that isClosed`, function () {
-            // given
-            const converter = new HoursConverter(jest.fn() as any, jest.fn() as any, jest.fn() as any);
+    describe("closedHours", function () {
+        [
+            "CLOSED",
+            "closed",
+            "Closed",
+        ].forEach(str =>
+            it(`should be able to recognized "${str}" and will return hours that isClosed`, function () {
+                // given
+                const converter = new HoursConverter(jest.fn() as any, jest.fn() as any, jest.fn() as any);
 
-            // when
-            const hours = converter.convert(str);
+                // when
+                const hours = converter.convert(str);
 
-            // then
-            expect(hours.isClosed()).toEqual(true);
-            expect(hours.asArray()).toEqual([]);
-        })
-    );
+                // then
+                expect(hours.isClosed()).toEqual(true);
+                expect(hours.asArray()).toEqual([]);
+            })
+        );
+    });
 });
