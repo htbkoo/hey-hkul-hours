315c8b88bd88169cbd73e8acf34db43acb677ed2
htbkoo <h1017760@connect.hku.hk>
2018-12-19 21:02:52 +0800

    updated HtmlFetcher.external.spec.ts to clarify that FIXED_DATE is used for test with expected response htbkoo/hey-hkul-hours

--- src/external/HtmlFetcher.external.spec.ts
+++ src/external/HtmlFetcher.external.spec.ts
@@ -1,10 +1,12 @@
 import HtmlFetcher from "./HtmlFetcher";
 import expectedText from "../tests/resources/external/expectedHtmlFetchResponse";
 
+const DATE_WITH_EXPECTED_RESPONSE = "2018-12-13";
+
 describe("HtmlFetcher (external tests)", function () {
-    it("should fetch from external html", function () {
+    it(`should fetch from external html for fixed date "${DATE_WITH_EXPECTED_RESPONSE} and retrieve expected response"`, function () {
         // given
-        const url = `https://lib.hku.hk/hours/daily/opening_hours_2018-12-13.html`;
+        const url = `https://lib.hku.hk/hours/daily/opening_hours_${DATE_WITH_EXPECTED_RESPONSE}.html`;
 
         // when
         const promise = new HtmlFetcher().fetchHtml(url);
