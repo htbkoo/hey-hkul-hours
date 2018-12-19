6b25c0ac08c9a85f341b28dfb13826c4c3ee6f04
htbkoo <h1017760@connect.hku.hk>
2018-12-19 22:46:23 +0800

    added nextDayHour in HourUtils.ts htbkoo/hey-hkul-hours

--- src/tests/utils/HourUtils.ts
+++ src/tests/utils/HourUtils.ts
@@ -4,3 +4,7 @@ import * as moment from "moment";
 export function hour(str: string): Moment {
     return moment(str, "h:ma");
 }
+
+export function nextDayHour(str: string): Moment {
+    return hour(str).add(1, "d");
+}
