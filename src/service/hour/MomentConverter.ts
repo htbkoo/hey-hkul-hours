05363cce2ff05104a6ea2fe022e1abf707ae3ebb
htbkoo <h1017760@connect.hku.hk>
2018-12-19 22:10:11 +0800

    refactored MomentConverter.ts htbkoo/hey-hkul-hours

--- src/service/hour/MomentConverter.ts
+++ src/service/hour/MomentConverter.ts
@@ -8,27 +8,27 @@ const DEFAULT_TIME_FORMAT = "h:ma";
 
 export default class MomentConverter {
     convert(str: string): Moment {
-        const parseResult = parsingAsFollowingDay();
+        const parseResult = this.parsingAsFollowingDay(str);
 
         if (parseResult.isFollowingDay()) {
             return parseResult.asMoment();
         } else {
             return moment(str, DEFAULT_TIME_FORMAT);
         }
+    }
 
-        function parsingAsFollowingDay() {
-            const result = FOLLOWING_DAY_REGEX.exec(str);
-            return {
-                isFollowingDay() {
-                    return result !== null && isNonEmpty(this.getTime());
-                },
-                asMoment() {
-                    return moment(this.getTime().trim(), DEFAULT_TIME_FORMAT).add(1, "d");
-                },
-                getTime(): string {
-                    return result[1];
-                }
-            };
-        }
+    private parsingAsFollowingDay(str) {
+        const result = FOLLOWING_DAY_REGEX.exec(str);
+        return {
+            isFollowingDay() {
+                return result !== null && isNonEmpty(this.getTime());
+            },
+            asMoment() {
+                return moment(this.getTime().trim(), DEFAULT_TIME_FORMAT).add(1, "d");
+            },
+            getTime(): string {
+                return result[1];
+            }
+        };
     }
 }
