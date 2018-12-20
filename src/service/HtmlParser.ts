2df052273c61a3e229c4e94213b9bd9c2dc00967
htbkoo <h1017760@connect.hku.hk>
2018-12-20 09:33:36 +0800

    refactored HtmlParser.ts htbkoo/hey-hkul-hours

--- src/service/HtmlParser.ts
+++ src/service/HtmlParser.ts
@@ -1,36 +1,35 @@
 import {JSDOM} from "jsdom";
 
 export default class HtmlParser {
-    constructor() {
+    parseHtml(html) {
+        return this.getAllRows(html)
+            .map(this.rowToTableCells)
+            .map(this.tableCellsToStrings)
+            .reduce(this.toRawStringsMap, {});
     }
 
-    parseHtml(html) {
-        return getAllRows()
-            .map(rowToTableCells)
-            .map(tableCellsToStrings)
-            .reduce((obj, {libraryName, hour}) => {
-                obj[libraryName as any] = hour;
-                return obj;
-            }, {});
+    private getAllRows(html) {
+        const dom = new JSDOM(html);
+        return Array.from(dom.window.document.querySelectorAll("tr")); // reference: https://stackoverflow.com/a/25657154
+    }
 
-        function getAllRows() {
-            const dom = new JSDOM(html);
-            return Array.from(dom.window.document.querySelectorAll("tr")); // reference: https://stackoverflow.com/a/25657154
+    private rowToTableCells(row) {
+        return Array.from((row as any).querySelectorAll("td"))
+    }
+
+    private tableCellsToStrings(tableCells) {
+        return {
+            libraryName: cellAsString(tableCells[0]),
+            hour: cellAsString(tableCells[1]),
+        };
+
+        function cellAsString(cell: any) {
+            return cell.textContent;
         }
+    }
 
-        function rowToTableCells(row) {
-            return Array.from((row as any).querySelectorAll("td"))
-        }
-
-        function tableCellsToStrings(tableCells) {
-            return {
-                libraryName: cellAsString(tableCells[0]),
-                hour: cellAsString(tableCells[1]),
-            };
-
-            function cellAsString(cell: any) {
-                return cell.textContent;
-            }
-        }
+    private toRawStringsMap(obj, {libraryName, hour}) {
+        obj[libraryName as any] = hour;
+        return obj;
     }
 }
