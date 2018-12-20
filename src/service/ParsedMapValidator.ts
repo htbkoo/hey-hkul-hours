66d4ea1e599b652b10197eab2c586c5f5900b60d
htbkoo <h1017760@connect.hku.hk>
2018-12-20 22:22:06 +0800

    implemented valid case for ParsedMapValidator.validate() htbkoo/hey-hkul-hours

--- src/service/ParsedMapValidator.ts
+++ src/service/ParsedMapValidator.ts
@@ -5,7 +5,18 @@ import {RawStringsMap} from "./validation/model/RawStringsMap";
 const preconditions = errr();
 
 export default class ParsedMapValidator {
-    validate(stringsMap: RawStringsMap) {
-        preconditions.shouldBeDefined(stringsMap[KEY_FOR_DATE], 'Missing mapping of "%s" for date from the input', KEY_FOR_DATE).test();
+    validate(stringsMap: RawStringsMap): ValidatedStringsMap {
+        const date = stringsMap[KEY_FOR_DATE];
+        preconditions.shouldBeDefined(date, 'Missing mapping of "%s" for date from the input', KEY_FOR_DATE).test();
+
+        const {[KEY_FOR_DATE]: _, ...hours} = stringsMap;
+        return {
+            getDate(): string {
+                return date;
+            },
+            getHoursMapping() {
+                return hours
+            }
+        }
     }
 }
