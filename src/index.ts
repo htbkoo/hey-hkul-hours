85563e5eda7ba3106fb79b88f0b492187cdca88e
htbkoo <h1017760@connect.hku.hk>
2018-12-23 14:38:00 +0800

    created src/index.ts as main entry (as library module) htbkoo/hey-hkul-hours

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import HtmlFetcher from "./external/HtmlFetcher";
+import LibraryHoursFetcher from "./fetcher/LibraryHoursFetcher";
+import HtmlParser from "./service/HtmlParser";
+import UrlAppender from "./service/UrlAppender";
+import ParsedMapValidator from "./service/ParsedMapValidator";
+import HkuLibraryHoursFactory from "./service/hour/HkuLibraryHoursFactory";
+import HoursConverter from "./service/HoursConverter";
+import SimpleHourParser from "./service/hour/SimpleHourParser";
+import SimpleHoursSplitter from "./service/hour/SimpleHoursSplitter";
+import MomentConverter from "./service/hour/MomentConverter";
+
+class HkuLibraryHoursFetcher extends LibraryHoursFetcher {
+    constructor() {
+        super(
+            new HtmlFetcher(),
+            new HtmlParser(),
+            new UrlAppender("https://lib.hku.hk/hours/daily/opening_hours_", ".html"),
+            new ParsedMapValidator(),
+            new HkuLibraryHoursFactory(
+                new HoursConverter(
+                    new SimpleHoursSplitter(),
+                    new SimpleHourParser(),
+                    new MomentConverter()
+                )
+            )
+        );
+    }
+}
+
+module.exports = {
+    HkuLibraryHoursFetcher,
+
+    HtmlFetcher,
+    LibraryHoursFetcher,
+    HtmlParser,
+    UrlAppender,
+    ParsedMapValidator,
+    HkuLibraryHoursFactory,
+    HoursConverter,
+    SimpleHourParser,
+    SimpleHoursSplitter,
+    MomentConverter,
+};
