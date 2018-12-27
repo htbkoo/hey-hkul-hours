0d2150e834137adebf5ef3ba0ac43e83866cc1dd
htbkoo <h1017760@connect.hku.hk>
2018-12-27 20:05:07 +0800

    hey-hkul-hours-web - fixed infinite "Invalid Host/Origin Header" warning problem (reference: https://github.com/webpack/webpack-dev-server/issues/1604#issue-393549402) htbkoo/hey-hkul-hours

--- app/web/vue/vue.config.js
+++ app/web/vue/vue.config.js
@@ -1,3 +1,31 @@
 module.exports = {
-    lintOnSave: false
+    lintOnSave: false,
+    devServer: {disableHostCheck: true},
 };
+
+
+/*
+
+
+    configureWebpack: config => {
+        if (isProduction()) {
+            // mutate config for production...
+            return config;
+        } else {
+            // mutate for development...
+            console.log(JSON.stringify(config));
+            if (!("devServer" in config)) {
+                config.devServer = {};
+            }
+            config.devServer.disableHostCheck = true;
+
+            return config;
+        }
+    }
+
+
+* */
+
+function isProduction() {
+    return process.env.NODE_ENV === 'production';
+}
