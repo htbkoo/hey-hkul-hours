0df075f37253119e4bc09ec0145f4d88594c7e95
htbkoo <h1017760@connect.hku.hk>
2018-12-27 20:05:23 +0800

    hey-hkul-hours-web - cleaned up and removed unused in `vue.config.js` htbkoo/hey-hkul-hours

--- app/web/vue/vue.config.js
+++ app/web/vue/vue.config.js
@@ -2,30 +2,3 @@ module.exports = {
     lintOnSave: false,
     devServer: {disableHostCheck: true},
 };
-
-
-/*
-
-
-    configureWebpack: config => {
-        if (isProduction()) {
-            // mutate config for production...
-            return config;
-        } else {
-            // mutate for development...
-            console.log(JSON.stringify(config));
-            if (!("devServer" in config)) {
-                config.devServer = {};
-            }
-            config.devServer.disableHostCheck = true;
-
-            return config;
-        }
-    }
-
-
-* */
-
-function isProduction() {
-    return process.env.NODE_ENV === 'production';
-}
