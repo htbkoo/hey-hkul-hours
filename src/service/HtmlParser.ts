e9b4efdda7a40d109faf80e287f37a06605d2cd9
htbkoo <h1017760@connect.hku.hk>
2019-01-04 22:32:42 +0800

    used `cheerio` instead of `jsdom` as htmlParsing library as `jsdom` is not supported in nativescript htbkoo/hey-hkul-hours

--- src/service/HtmlParser.ts
+++ src/service/HtmlParser.ts
@@ -1,36 +1,15 @@
-import {JSDOM} from "jsdom";
+import * as cheerio from "cheerio";
+
 import {RawStringsMap} from "./validation/model/RawStringsMap";
 
 export default class HtmlParser {
     parseHtml(html: string): RawStringsMap {
-        return this.getAllRows(html)
-            .map(this.rowToTableCells)
-            .map(this.tableCellsToStrings)
-            .reduce(this.toRawStringsMap, {});
-    }
+        const $ = cheerio.load(html);
 
-    private getAllRows(html) {
-        const dom = new JSDOM(html);
-        return Array.from(dom.window.document.querySelectorAll("tr")); // reference: https://stackoverflow.com/a/25657154
-    }
-
-    private rowToTableCells(row) {
-        return Array.from((row as any).querySelectorAll("td"))
-    }
-
-    private tableCellsToStrings(tableCells) {
-        return {
-            libraryName: cellAsString(tableCells[0]),
-            hour: cellAsString(tableCells[1]),
-        };
-
-        function cellAsString(cell: any) {
-            return cell.textContent;
-        }
-    }
-
-    private toRawStringsMap(obj, {libraryName, hour}) {
-        obj[libraryName as any] = hour;
-        return obj;
+        return Array.from($("tr").map((i, tr) => $(tr).find("td").map((i, td) => $(td).text())))
+            .reduce((obj, node) => {
+                obj[node[0]] = node[1];
+                return obj;
+            }, {});
     }
 }
